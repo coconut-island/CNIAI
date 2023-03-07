ba264db673d13bbcb4c3d13b70b0c78c5ba1f15e
abelleeye <abel@abellee.cn>
2023-03-07 19:46:04 +0800

    [FEAT/FIX] addd rgb2bgr_resize_bilinear_pad_norm cuda kernel and fix output value

--- cniai/cniai_cuda_kernel/include/cniai_cuda_kernel/preprocess.h
+++ cniai/cniai_cuda_kernel/include/cniai_cuda_kernel/preprocess.h
@@ -55,6 +55,74 @@ void rgb_resize_bilinear_pad_norm_output_planar(const uint8_t *src, float *dst,
                                                 float mean0, float mean1, float mean2,
                                                 float std0, float std1, float std2, cudaStream_t cudaStream);
 
+/**
+ *
+ * @param src src image packed format
+ * @param dst dst image packed format
+ * @param src_width
+ * @param src_height
+ * @param img_width resized image width
+ * @param img_height resized image height
+ * @param dst_width dst image width
+ * @param dst_height  dst image height
+ * @param img_x resized image start x in dst image
+ * @param img_y resized image start y in dst image
+ * @param pad0 src channel 0 pad, if src is rgb, here is r. if src is bgr, here is b.
+ * @param pad1 src channel 1 pad
+ * @param pad2 src channel 2 pad
+ * @param scale
+ * @param mean0 src channel 0 mean
+ * @param mean1 src channel 1 mean
+ * @param mean2 src channel 2 mean
+ * @param std0 src channel 0 std
+ * @param std1 src channel 1 std
+ * @param std2 src channel 2 std
+ * @param cudaStream
+ */
+void rgb2bgr_resize_bilinear_pad_norm(const uint8_t *src, float *dst,
+                                      const int src_width, const int src_height,
+                                      const int img_width, const int img_height,
+                                      const int dst_width, const int dst_height,
+                                      const int img_x, const int img_y,
+                                      const int pad0, const int pad1, const int pad2,
+                                      float scale,
+                                      float mean0, float mean1, float mean2,
+                                      float std0, float std1, float std2, cudaStream_t cudaStream);
+
+/**
+ *
+ * @param src src image packed format
+ * @param dst dst image planar format
+ * @param src_width
+ * @param src_height
+ * @param img_width resized image width
+ * @param img_height resized image height
+ * @param dst_width dst image width
+ * @param dst_height  dst image height
+ * @param img_x resized image start x in dst image
+ * @param img_y resized image start y in dst image
+ * @param pad0 src channel 0 pad, if src is rgb, here is r. if src is bgr, here is b.
+ * @param pad1 src channel 1 pad
+ * @param pad2 src channel 2 pad
+ * @param scale
+ * @param mean0 src channel 0 mean
+ * @param mean1 src channel 1 mean
+ * @param mean2 src channel 2 mean
+ * @param std0 src channel 0 std
+ * @param std1 src channel 1 std
+ * @param std2 src channel 2 std
+ * @param cudaStream
+ */
+void rgb2bgr_resize_bilinear_pad_norm_output_planar(const uint8_t *src, float *dst,
+                                                    const int src_width, const int src_height,
+                                                    const int img_width, const int img_height,
+                                                    const int dst_width, const int dst_height,
+                                                    const int img_x, const int img_y,
+                                                    const int pad0, const int pad1, const int pad2,
+                                                    float scale,
+                                                    float mean0, float mean1, float mean2,
+                                                    float std0, float std1, float std2, cudaStream_t cudaStream);
+
 }
 }
 
--- cniai/cniai_cuda_kernel/src/rgb_resize_pad_norm.cu
+++ cniai/cniai_cuda_kernel/src/rgb_resize_pad_norm.cu
@@ -9,7 +9,7 @@
 namespace cniai {
 namespace preprocess {
 
-template<int c = 3, bool is_output_planar>
+template<int c = 3, bool is_output_planar = false, bool is_swap_r_b = false>
 __global__ void rgb_resize_bilinear_pad_norm_kernel(const uint8_t *src, float *dst,
                                                const int src_width, const int src_height,
                                                const int img_width, const int img_height,
@@ -31,7 +31,7 @@ __global__ void rgb_resize_bilinear_pad_norm_kernel(const uint8_t *src, float *d
 
     bool is_in_img = img_y <= dst_y && dst_y < img_y + img_height && img_x <= dst_x && dst_x < img_x + img_width;
     for (int c_idx = 0; c_idx < c; c_idx++) {
-        uint8_t out = 0;
+        float out = 0;
         if (is_in_img) {
             const int x1 = __float2int_rd(src_x);
             const int y1 = __float2int_rd(src_y);
@@ -60,9 +60,15 @@ __global__ void rgb_resize_bilinear_pad_norm_kernel(const uint8_t *src, float *d
             out = c_idx == 0 ? pad0 : c_idx == 1 ? pad1 : pad2;
         }
 
+
+        int cur_c_idx = c_idx;
+        if (is_swap_r_b) {
+            cur_c_idx = c_idx == 0 ? 2 : c_idx == 2 ? 0 : 1;
+        }
+
         int dst_current_idx = is_output_planar ?
-                              dst_width * dst_height * c_idx + dst_y * dst_width + dst_x :
-                              dst_y * dst_width * c + dst_x * c + c_idx;
+                              dst_width * dst_height * cur_c_idx + dst_y * dst_width + dst_x :
+                              dst_y * dst_width * c + dst_x * c + cur_c_idx;
 
         dst[dst_current_idx] = out;
 
@@ -118,6 +124,57 @@ void rgb_resize_bilinear_pad_norm_output_planar(const uint8_t *src, float *dst,
                                                         scale,
                                                         mean0, mean1, mean2,
                                                         std0, std1, std2, scale_x, scale_y);
+}
+
+
+void rgb2bgr_resize_bilinear_pad_norm(const uint8_t *src, float *dst,
+                                  const int src_width, const int src_height,
+                                  const int img_width, const int img_height,
+                                  const int dst_width, const int dst_height,
+                                  const int img_x, const int img_y,
+                                  const int pad0, const int pad1, const int pad2,
+                                  float scale,
+                                  float mean0, float mean1, float mean2,
+                                  float std0, float std1, float std2, cudaStream_t cudaStream) {
+    dim3 block(32, 32);
+    dim3 grid((dst_width + block.x - 1) / block.x, (dst_height + block.y - 1) / block.y);
+    float scale_x = static_cast<float>(src_width) / img_width;
+    float scale_y = static_cast<float>(src_height) / img_height;
+
+    rgb_resize_bilinear_pad_norm_kernel<3, false, true><<<grid, block, 0, cudaStream>>>(
+            src, dst, src_width, src_height,
+            img_width, img_height,
+            dst_width, dst_height, img_x, img_y,
+            pad0, pad1, pad2,
+            scale,
+            mean0, mean1, mean2,
+            std0, std1, std2,
+            scale_x, scale_y);
+}
+
+
+void rgb2bgr_resize_bilinear_pad_norm_output_planar(const uint8_t *src, float *dst,
+                                                const int src_width, const int src_height,
+                                                const int img_width, const int img_height,
+                                                const int dst_width, const int dst_height,
+                                                const int img_x, const int img_y,
+                                                const int pad0, const int pad1, const int pad2,
+                                                float scale,
+                                                float mean0, float mean1, float mean2,
+                                                float std0, float std1, float std2, cudaStream_t cudaStream) {
+    dim3 block(32, 32);
+    dim3 grid((dst_width + block.x - 1) / block.x, (dst_height + block.y - 1) / block.y);
+    float scale_x = static_cast<float>(src_width) / img_width;
+    float scale_y = static_cast<float>(src_height) / img_height;
+
+    rgb_resize_bilinear_pad_norm_kernel<3, true, true><<<grid, block, 0, cudaStream>>>(
+            src, dst, src_width, src_height,
+            img_width, img_height,
+            dst_width, dst_height, img_x, img_y,
+            pad0, pad1, pad2,
+            scale,
+            mean0, mean1, mean2,
+            std0, std1, std2, scale_x, scale_y);
 }
 
 }
