f3c9eb5624b2c83c06b3ec17d3cfae63f087a9f7
abelleeye <abel@abellee.cn>
2023-05-13 12:38:54 +0800

    [Refactor] refactor all code format / style

--- cniai/src/cniai.cc
+++ cniai/src/cniai.cc
@@ -4,8 +4,8 @@
 #include "common/logging.h"
 #include <gflags/gflags.h>
 
-DEFINE_string(log_level, "info", "Log level, includes [trace, debug, info, warn, err, critical, off]");
-DEFINE_string(log_pattern, "[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [%t] [%@] %v", "Log pattern");
+DEFINE_string(LOG_LEVEL, "info", "Log level, includes [trace, debug, info, warn, err, critical, off]");
+DEFINE_string(LOG_PATTERN, "[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [%t] [%@] %v", "Log pattern");
 
 
 int main(int argc, char *argv[]) {
@@ -13,8 +13,8 @@ int main(int argc, char *argv[]) {
     gflags::SetVersionString("0.0.1");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-    SET_LOG_PATTERN(FLAGS_log_pattern);
-    SET_LOG_LEVEL(FLAGS_log_level);
+    SET_LOG_PATTERN(FLAGS_LOG_PATTERN);
+    SET_LOG_LEVEL(FLAGS_LOG_LEVEL);
 
     LOG_INFO("Hello cniai!");
 
